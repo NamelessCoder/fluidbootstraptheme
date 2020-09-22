84760a3bd7ccd87b5c4551168b423d266d797d5d
Claus Due <claus@namelesscoder.net>
2020-09-22 21:10:05 +0200

    [TASK] Adjust dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,9 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.1.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.00-8.7.99',
+			'typo3' => '7.6.00-10.4.99',
 			'flux' => '',
-			'fluidpages' => '',
 			'vhs' => '',
 		),
 		'conflicts' => array(
