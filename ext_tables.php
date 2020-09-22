44a6f55ad68fd2e8c94c3f0d48cd97fd86bcdc70
Claus Due <claus@namelesscoder.net>
2020-09-22 21:12:27 +0200

    [TASK] Move TS registration to sys_template override

--- ext_tables.php
+++ ext_tables.php
@@ -5,8 +5,6 @@ if (!defined('TYPO3_MODE')) {
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fluidbootstraptheme']['setup'] = unserialize($_EXTCONF);
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Bootstrap Theme');
-
 if (FALSE === (boolean) $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['fluidbootstraptheme']['setup']['disablePageTemplates']) {
 	\FluidTYPO3\Flux\Core::registerProviderExtensionKey('FluidBT.Fluidbootstraptheme', 'Page');
 }
